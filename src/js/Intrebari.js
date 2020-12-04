aceffc6fb683b2464846a01bd23f17655075ff84
Catalin-web <catalin.bugnar@yahoo.ro>
2020-12-04 16:45:47 +0200

    style

--- src/js/Intrebari.js
+++ src/js/Intrebari.js
@@ -4,12 +4,13 @@ import React from 'react';
 function Intrebari() {
 	return (
 		<>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Exercitationem suscipit quae vero beatae laborum praesentium
-				quibusdam atque animi! Quo, eos! Obcaecati numquam laudantium quas
-				exercitationem ut quisquam dolor? Corporis, explicabo.
-			</p>
+			<div className='indiv'>
+				<div className='intrebari'>
+					<div className='intrebaridiv'>
+						<span>Intrebare ?????</span>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
